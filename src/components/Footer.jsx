2a9ce3560e1f71d102aae072079c53881b4ddfd3
ThomasSmidt <smidt592@gmail.com>
2025-05-26 13:24:55 +0200

    minor visual changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <footer class="footer">
+            <p>blabla</p>
+        </footer>
+    );
+};
+
+export default Footer;
