15121687ea81ec6d50e10f696e659b4ee3cc9c44
ThomasSmidt <106915132+ThomasSmidt@users.noreply.github.com>
2025-05-28 16:50:13 +0200

    Fixed routing, readme, title and favicon

--- src/components/ProjectsComponent.jsx
+++ src/components/ProjectsComponent.jsx
@@ -7,21 +7,21 @@ const skillDescriptions = {
         date: '2022',
         technologies: ['HTML', 'CSS', 'JavaScript'],
         description: 'A simple blackjack game I made, when I was first learning web development.',
-        link: '/blackjack',
+        link: `${process.env.PUBLIC_URL}/blackjack/`,
         linkText: 'Try it!'
     },
     'Fast Food Ordering UI': {
         date: '2024',
         technologies: ['React', 'TypeScript', 'HTML', 'CSS'],
         description: 'A simple fast food ordering UI, which was made as part of a school project.',
-        link: '/fastfood',
+        link: 'https://github.com/ThomasSmidt/Simple-Fast-Food-Ordering-UI',
         linkText: 'Github Link'
     },
     'Portfolio Website': {
         date: '2025',
         technologies: ['React', 'TypeScript', 'HTML', 'CSS'],
         description: 'This portfolio website, showcasing my skills and projects.',
-        link: '/fastfood',
+        link: 'https://github.com/ThomasSmidt/thomas-smidt-website',
         linkText: 'Github Link'
     }
     
